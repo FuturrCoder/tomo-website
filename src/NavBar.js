f44bb5aa2306466234b84263dcd9f58af0e750d0
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2024-11-10 17:12:38 -0600

    change a to Link

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,21 @@
 import logo from "./logo.svg";
+import {Link} from "react-router-dom";
 
 function NavBar(props) {
   return (
     <nav
       className="flex flex-wrap justify-center items-center gap-4 border-b-2 border-b-gray-200 px-7 py-3 font-display">
-      <a href="/">
+      <Link to="/">
         <img src={logo} width="40" alt="logo"/>
-      </a>
-      <a className="text-4xl font-extrabold text-primary" href="/">tomo</a>
+      </Link>
+      <Link className="text-4xl font-extrabold text-primary" to="/">tomo</Link>
       <span className="sm:flex-grow"></span>
       <div className="block md:inline">
         {props.links.map((link) => (
-          <a className="text-2xl text-neutral-gray font-bold hover:text-primary [&:not(:first-child)]:ml-8"
-             href={link.url}>
+          <Link className="text-2xl text-neutral-gray font-bold hover:text-primary [&:not(:first-child)]:ml-8"
+             to={link.url}>
             {link.title}
-          </a>
+          </Link>
         ))}
       </div>
     </nav>
