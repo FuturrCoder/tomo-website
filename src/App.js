2c8eceb6afe4babd66baa1723103925e8f798575
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2024-11-14 17:27:25 -0600

    reorganize files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import PageLayout from "./PageLayout";
+import PageLayout from "./navigation/PageLayout";
 import Classes from "./classes/Classes";
 import Class from "./classes/class/Class";
 
--- src/navigation/PageLayout.js
+++ src/navigation/PageLayout.js
@@ -1,6 +1,6 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import NAV_LINKS from "./config/navbar.config";
+import NAV_LINKS from "../config/navbar.config";
 import {Outlet} from "react-router-dom";
 
 function PageLayout() {
